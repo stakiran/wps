1def59f5989a65d8ac745d2d25e0be2d528bfbda
stakiran <stakiran@users.noreply.github.com>
2017-09-14 15:11:45 +0900

    first commit.

---
+++ README.md
@@ -0,0 +1,155 @@
+# wps
+起動しているプロセスのコマンドライン一覧を見るコマンド(wmic processのラッパー)。
+
+<!-- toc -->
+- [wps](#wps)
+  - [Overview](#overview)
+  - [Demo](#demo)
+    - [見る](#見る)
+    - [殺す](#殺す)
+  - [Requirement](#requirement)
+  - [FAQ](#faq)
+    - [Q: Kill と Terminate の違いは？](#q-kill-と-terminate-の違いは)
+    - [Q: 出力フォーマットには何がありますか？](#q-出力フォーマットには何がありますか)
+    - [Q: 出力される PID って桁揃えとか入ってます？](#q-出力される-pid-って桁揃えとか入ってます)
+  - [License](#license)
+  - [Author](#author)
+
+## Overview
+起動しているプロセスのコマンドラインを知りたい時があるのですが、
+
+- タスクマネージャや Process Explorer など GUI → 少々扱いづらい
+- `tasklist` コマンド → コマンドラインが出ないので論外
+- `wmic process` コマンド → 引数が煩雑なのと、出力に余計な空行が入って見辛い
+
+というわけで何か良い手段は無いかと考えて、「個人的に使う引数も限られてるし wmic process の簡単なラッパーでいいか」と思い、書いてみたところ、意外と使えそうだったので公開します。
+
+## Demo
+
+### 見る
+引数無しで実行。
+
+```
+$ python wps.py
+    0 
+    4 
+  292 \SystemRoot\System32\smss.exe
+  580 wininit.exe
+  640 C:\Windows\system32\services.exe
+  828 "C:\Program Files\Mozilla Firefox\firefox.exe"
+  836 C:\Windows\system32\svchost.exe -k DcomLaunch
+...
+```
+
+出力フォーマット指定。 (PIDとキャプションとコマンドライン)
+
+```
+$ python wps.py -f "p c l"
+    0 System Idle Process 
+    4 System 
+  292 smss.exe \SystemRoot\System32\smss.exe
+  580 wininit.exe wininit.exe
+  640 services.exe C:\Windows\system32\services.exe
+  828 firefox.exe "C:\Program Files\Mozilla Firefox\firefox.exe"
+  836 svchost.exe C:\Windows\system32\svchost.exe -k DcomLaunch
+...
+```
+
+出力フォーマット指定その2。 (PIDと生成日時とキャプション)
+
+```
+$ python wps.py -f "p d c" --desc
+    0 2017/09/11 08:20:03 System Idle Process
+    4 2017/09/11 08:20:03 System
+  292 2017/09/11 08:20:03 smss.exe
+  580 2017/09/11 08:21:27 wininit.exe
+  640 2017/09/11 08:21:28 services.exe
+  828 2017/09/12 08:25:57 firefox.exe
+  836 2017/09/11 08:21:29 svchost.exe
+...
+```
+
+生成時刻で降順ソート。 (--descが無い場合は昇順)
+
+```
+$ python wps.py -f "d c" --desc
+2017/09/14 11:37:00 cmd.exe
+2017/09/14 11:37:00 WmiPrvSE.exe
+2017/09/14 11:37:00 WMIC.exe
+2017/09/14 11:36:59 python.exe
+2017/09/14 11:36:59 clip.exe
+2017/09/14 11:26:37 Hidemaru.exe
+2017/09/14 11:04:53 python.exe
+...
+```
+
+### 殺す
+PID を指定してプロセスを殺すことができる。以下は自分で立ち上げた notepad.exe を試しに殺してみた例。
+
+```
+$ notepad
+
+$ python wps.py -f "p l" notepad
+ 7048 notepad.exe
+
+$ python wps.py -k 7048
+成功: PID 7048 のプロセスに強制終了のシグナルを送信しました。
+
+$ python wps.py -f "p l" notepad
+
+$ python wps.py -k 7048
+エラー: プロセス "7048" が見つかりませんでした。
+```
+
+`-k` ではなく `-t` を使うと Terminate(強制終了) を実行する。
+
+## Requirement
+- Windows 7+
+- Python 2.7
+
+## FAQ
+
+### Q: Kill と Terminate の違いは？
+A: たとえば「編集されたメモ帳ウィンドウ」を殺す場合、Kill だと「保存しますか？」ダイアログが出るだけですが、Terminate だとダイアログも出ずに強制終了します。
+
+### Q: 出力フォーマットには何がありますか？
+A: 以下が使えます。
+
+- p : PID
+- l : command Line。コマンドライン
+- d : creation Date。プロセス生成日時
+- c : Caption。タイトル
+
+たとえば PID、コマンドラインの順に表示したい場合は `python wps.py -f "p l"` のように指定します。
+
+Usage にもしれっと書いてます。
+
+```
+$ python wps.py -h
+usage: wps.py [-h] [-f FORMAT] [--desc] [-k KILL] [-t TERMINATE]
+              [keyword [keyword ...]]
+
+positional arguments:
+  keyword               Keywords to extract lines with lowercase-AND partial
+                        search. (default: )
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f FORMAT, --format FORMAT
+                        A combination of p=PID, d=creationDate, c=Caption and
+                        l=commandLine (default: p l)
+  --desc                Use DESC order. If not given, use ASC order. (default:
+                        False)
+  -k KILL, --kill KILL  A PID being killed. (default: None)
+  -t TERMINATE, --terminate TERMINATE
+                        A PID being killed forcely. (default: None)
+```
+
+### Q: 出力される PID って桁揃えとか入ってます？
+A: 入ってます。5桁で揃えてます。コマンドラインオプションを単純にするため5桁揃え固定にしています。
+
+## License
+[MIT License](LICENSE)
+
+## Author
+[stakiran](https://github.com/stakiran)
---
+++ wps.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+
+import os
+import subprocess
+
+def get_stdout(cmdline):
+    return subprocess.check_output(cmdline, shell=True)
+
+def p(msg):
+    print msg
+
+def abort(msg):
+    p('Error:{:}'.format(msg))
+    exit(1)
+
+def parse_arguments():
+    import argparse
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('keyword', default='', nargs='*',
+        help='Keywords to extract lines with lowercase-AND partial search.')
+
+    parser.add_argument('-f', '--format', default='p l', type=str,
+        help='A combination of p=PID, d=creationDate, c=Caption and l=commandLine')
+    parser.add_argument('--desc', default=False, action='store_true',
+        help='Use DESC order. If not given, use ASC order.')
+
+    parser.add_argument('-k', '--kill', default=None, type=int,
+        help='A PID being killed.')
+    parser.add_argument('-t', '--terminate', default=None, type=int,
+        help='A PID being killed forcely.')
+
+    return parser.parse_args()
+
+args = parse_arguments()
+keywords     = args.keyword
+line_format = args.format
+use_desc    = args.desc
+kill_pid    = args.kill
+term_pid    = args.terminate
+
+if kill_pid:
+    returncode = os.system('taskkill /pid {:}'.format(kill_pid))
+    exit(returncode)
+if term_pid:
+    returncode = os.system('taskkill /f /pid {:}'.format(term_pid))
+    exit(returncode)
+
+selfpid = os.getpid()
+
+attrs = 'COMMANDLINE,PROCESSID,CREATIONDATE,CAPTION'
+attrs_count = len(attrs.split(','))
+commandline = 'wmic process get {:} /FORMAT:LIST'.format(attrs)
+
+stdout_raw = get_stdout(commandline)
+stdout_lines = stdout_raw.split('\r\n')
+# remove blank lines.
+stdout_lines = [line for line in stdout_lines if len(line.strip())!=0]
+# remove each lines's line-break. (Not \r\n but \n)
+stdout_lines = [line[:-1] for line in stdout_lines]
+
+# [Original Format]
+# Caption=PyScripter.exe
+# CommandLine="D:\bin\PyScripter\PyScripter.exe" "D:\work\github\stakiran\wps\wps.py"
+# CreationDate=20170914094116.104179+540
+# ProcessId=3120
+class Process:
+    def __init__(self):
+        pass
+
+    def parse_line(self, line):
+        if line.find('=')==-1:
+            abort('Invalid "wmic process" format: "{:}"'.format(line))
+        k, v = line.split('=', 1)
+
+        k = k.lower()
+        setter = None
+        setter_dict = {
+            'caption'      : self.set_caption,
+            'commandline'  : self.set_commandline,
+            'creationdate' : self.set_date,
+            'processid'    : self.set_pid,
+        }
+        setter = setter_dict[k]
+        setter(v)
+
+    def set_caption(self, caption):
+        self._caption = caption
+
+    def set_commandline(self, commandline):
+        self._commandline = commandline
+
+    def set_date(self, creationdate):
+        d = creationdate
+
+        # BEFORE: 20170914094116.104179+540
+        # AFTER : 2017/09/14 09:41:16
+
+        self._date = '{:}/{:}/{:} {:}:{:}:{:}'.format(
+            d[0:4], d[4:6], d[6:8], d[8:10], d[10:12], d[12:14]
+        )
+
+    def set_pid(self, pid_by_str):
+        self._pid = int(pid_by_str)
+
+    @property
+    def pid(self):
+        return self._pid
+
+    @property
+    def commandline(self):
+        return self._commandline
+
+    @property
+    def caption(self):
+        return self._caption
+
+    @property
+    def date(self):
+        return self._date
+
+processes = []
+process = None
+for i,line in enumerate(stdout_lines):
+    if i%attrs_count==0:
+        if i!=0:
+            processes.append(process)
+        process = Process()
+    process.parse_line(line)
+processes.append(process) # The last instance.
+
+outlines = []
+for i,process in enumerate(processes):
+    # 自身のプロセス情報は除外する.
+    if process.pid == selfpid:
+        continue
+
+    line = line_format
+    # 直に replace すると他の値まで置換しちゃうので
+    # まずは「どの値にも現れないであろう文字」を入れた文字列に置換して,
+    # それからそいつを置換することで回避.
+    line = line.replace('p' , '|p|')
+    line = line.replace('d' , '|d|')
+    line = line.replace('c' , '|c|')
+    line = line.replace('l' , '|l|')
+    line = line.replace('|p|', '{:>5}'.format(process.pid))
+    line = line.replace('|d|', process.date)
+    line = line.replace('|c|', process.caption)
+    line = line.replace('|l|', process.commandline)
+    outlines.append(line)
+
+outlines.sort()
+if use_desc:
+    outlines.reverse()
+
+def be_extracted(line, keywords):
+    # All extrcted because no keyword.
+    if len(keywords)==0:
+        return True
+
+    filteree = line.lower()
+    for keyword in keywords:
+        q = keyword.lower()
+        if filteree.find(q)==-1:
+            return False
+    return True
+
+for line in outlines:
+    if be_extracted(line, keywords):
+        p(line)
+
+exit(0)
+
